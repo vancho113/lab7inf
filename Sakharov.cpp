7810ccda99931332e8d04dbd4b94ae329ac019f5
vancho113 <vasya.games.channel555@gmail.com>
2024-12-13 00:02:17 +0300

    Add files via upload

---
+++ Sakharov.cpp
@@ -0,0 +1,12 @@
+﻿#include <iostream>
+#include <stdlib.h>
+#include <conio.h> // подгрузка для нахождения псевдорандомных чисел
+#include <locale>
+#include <string.h>
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << 1 + 1;
+	return 0;
+}
